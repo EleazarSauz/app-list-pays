3982640e10a366dfde270cdae7ff4282d5fca710
Eleazar <eleazar48a@gmail.com>
2020-12-20 01:04:24 -0600

    add tailwind css

---
+++ front/src/components/form.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <div>
+            <section className="text-gray-700 body-font relative">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-12">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Add new pay</h1>
+                    </div>
+                    <div className=" mx-auto max-w-xl">
+                        <div className="flex flex-wrap -m-2">
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Creator</label>
+                                    <input type="text" id="name" name="name" className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                </div>
+                            </div>
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label htmlFor="email" className="leading-7 text-sm text-gray-600">Kind</label>
+                                    <input type="email" id="email" name="email" className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                </div>
+                            </div>
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Amount value</label>
+                                    <input type="text" id="name" name="name" className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                </div>
+                            </div>
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Status</label>
+                                    <input type="text" id="name" name="name" className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                </div>
+                            </div>
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Provider</label>
+                                    <input type="text" id="name" name="name" className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                </div>
+                            </div>
+                            
+                            <div className="p-2 w-full">
+                                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add</button>
+                            </div>
+                            <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                                <p className="leading-normal my-5">
+                                    Made with ❤️
+                                    <br />
+                                    By Eleazar Saúz 🙋‍♂️
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Form
